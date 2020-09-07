b322eb2b9904a90347f08d489c97cc15bf406ae4
wilsjame <jgwilson1214@gmail.com>
2020-09-06 22:12:55 -0700

    minimizing coins

---
+++ cses/minimizing-coins.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+const int INF = 1e9 + 5;
+int main() {
+    int n, x;
+    cin >> n >> x;
+    vector<int> coins(n);
+    for (auto &c : coins)
+        cin >> c;
+
+    vector<int> dp(x + 1, INF); 
+    dp[0] = 0; // the minimum number of coins to construct sum
+    for (int sum = 1; sum <= x; sum++) {
+        for (int c : coins) {
+            if (sum - c >= 0) {
+                dp[sum] = min(dp[sum], dp[sum - c] + 1);
+            }
+        }
+    }
+    dp[x] == INF ? cout << -1 : cout << dp[x];
+
+    return 0;
+}
