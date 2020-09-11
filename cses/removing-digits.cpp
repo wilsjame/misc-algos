d3cf5b8ed8d0f23cbb18cb0602e0177097e3f657
wilsjame <jgwilson1214@gmail.com>
2020-09-10 21:12:47 -0700

    solve removing digits

--- cses/removing-digits.cpp
+++ cses/removing-digits.cpp
@@ -6,25 +6,20 @@ int main() {
     int n;
     cin >> n;
 
-    // recursive thinking (top down)
-    // subtract every digit creating a tree all the way down
-    // keep on subtracting every digit until zero is reached.
-    // choose shortest path to zero
-
-    // bottom up
-    // 1 <= x <= n (target)
     // steps[x] is the minimum steps required to make x equal to zero
     vector<int> steps(n + 1);
-    // base cases: 
+    // base cases 
     steps[0] = 0;
     for (int i = 1; i <= 9; i++) 
         steps[i] = 1;
 
     for (int x = 10; x <= n; x++) {
         steps[x] = INF;
-        for (int digit : /* in x */) {
+        // for each digit in x
+        int k = x;
+        for (int digit = k % 10; k > 0; k /= 10, digit = k % 10) {
             if (x - digit >= 0) {
-                steps[x] = min(steps[x], step[x - digit] + 1);
+                steps[x] = min(steps[x], steps[x - digit] + 1);
             }
         }
     }
