2257772652c79832b910ccb58f3b3504f54d4e24
wilsjame <jgwilson1214@gmail.com>
2020-09-10 21:39:21 -0700

    spacing ;_;

--- cses/removing-digits.cpp
+++ cses/removing-digits.cpp
@@ -6,7 +6,6 @@ const int INF = 1e9 + 5;
 int main() {
     int n;
     cin >> n;
-
     // steps[x] is the minimum steps required to make x equal to zero
     vector<int> steps(n + 1);
     // base cases 
