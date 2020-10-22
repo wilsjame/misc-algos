7eaff0a8c151985fe0e3f3aec8a4767c32e77e09
James Wilson <jgwilson1214@gmail.com>
2020-10-21 21:13:45 -0700

    refactor

--- cses/coin-piles.cpp
+++ cses/coin-piles.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-#include <cmath>
 #include <algorithm>
 using namespace std;
 
@@ -9,21 +8,11 @@ int main() {
 	while (T--) {
 		int A, B;
 		cin >> A >> B;
-		int a, b, z;
-		a = A % 3;
-		b = B % 3;
-		z = abs(A - B);
-		if (z > min(A, B)) {
-			cout << "NO\n";
-		}
-		else {
-			if (A == 0 && B == 0) 
-				cout << "YES\n";
-			else if ((A > 0 && B > 0) && ((a == 0 && b == 0) || (a == 2 && b == 1) || (a == 1 && b == 2)))
-				cout << "YES\n";
-			else 
-				cout << "NO\n";
-		}
+		int s, mn, mx;
+		s = A + B;
+		mn = min(A, B);
+		mx = max(A, B);
+		s % 3 == 0 && 2 * mn >= mx ? cout << "YES\n" : cout << "NO\n";
 	}
 
 	return 0;
