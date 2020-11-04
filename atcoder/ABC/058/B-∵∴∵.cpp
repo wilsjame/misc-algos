9f53b6a0ee5ff601440e49f4a6d0ff4f73f7857b
wilsjame <jgwilson1214@gmail.com>
2020-11-04 00:09:19 -0800

    refactor

--- atcoder/ABC/058/B-∵∴∵.cpp
+++ atcoder/ABC/058/B-∵∴∵.cpp
@@ -1,25 +1,15 @@
 #include <iostream>
 #include <string>
-#include <algorithm>
 using namespace std;
 
 int main() {
     string O, E;
     cin >> O >> E;
-    reverse(O.begin(), O.end());
-    reverse(E.begin(), E.end());
-    string ans;
-    while (!O.empty() || !E.empty()) {
-        if (!O.empty()) {
-            ans += O.back();
-            O.pop_back();
-        }
-        if (!E.empty()) {
-            ans += E.back();
-            E.pop_back();
-        }
+    for (int i = 0; i < O.size(); i++) {
+        cout << O[i];
+        if (i < E.size())
+            cout << E[i];
     }
-    cout << ans;
-
+    
     return 0;
 }
