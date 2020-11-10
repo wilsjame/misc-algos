861391fa93f18084008b1aa4c2e59adaae40d2c0
James Wilson <jgwilson1214@gmail.com>
2020-11-09 23:31:11 -0800

    overkill, use dp

--- atcoder/ABC/063/C-Bugged.cpp
+++ atcoder/ABC/063/C-Bugged.cpp
@@ -1,9 +1,42 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <cstdio>
 using namespace std;
 
 int main() {
+    int N;
+    cin >> N;
+    vector<int> S(N + 1);
+    for (int i = 1; i <= N; i++) {
+        cin >> S[i];
+    }
+
+    int dp[101][10005] = {};
+    dp[0][0] = 1;
+    for (int i = 1; i <= N; i++) {
+        for (int j = 0; j <= 10000; j++) {
+            dp[i][j] = dp[i - 1][j];
+            if (j - S[i] >= 0 && dp[i - 1][j - S[i]] == 1)
+                dp[i][j] = 1;
+            //printf("dp[%d][%d] = %d\n", i, j, dp[i][j]);
+        }
+    }
+
+    int ans = 0;
+    for (int score = 10000; score >= 0; score--) {
+        if (dp[N][score] == 1 && score % 10 != 0) {
+            ans = score;
+            break;
+        }
+    }
+    cout << ans << endl;
+
+    return 0;
+}
+
+// straight-forward
+/*
     int N, sum = 0;;
     cin >> N;
     vector<int> A(N);
@@ -24,6 +57,4 @@ int main() {
         cout << 0 << endl;
     else 
         cout << ans << endl;
-
-    return 0;
-}
+*/
