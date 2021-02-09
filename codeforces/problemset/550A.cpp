4ed1b3fe299e887a2da5551e296b92634cfed2e5
wilsjame <jgwilson1214@gmail.com>
2021-02-08 19:00:41 -0800

    refactor

--- codeforces/problemset/550A.cpp
+++ codeforces/problemset/550A.cpp
@@ -1,25 +1,32 @@
 #include <iostream>
 #include <string>
+#include <vector>
+#include <algorithm>
 using namespace std;
 int main() {
     string s;
     cin >> s;
-    bool checked_a = false, checked_b = false;
+    vector<int> a, b;
     for (int i = 0; i + 1 < s.length(); i++) {
-        if (checked_a && checked_b) break;
-        string a = string() + s[i] + s[i+1];
-        if ((a == "AB" && !checked_a) || (a == "BA" && !checked_b)) {
-            if (a == "AB") checked_a = true;
-            if (a == "BA") checked_b = true;
-            for (int j = i + 2; j + 1 < s.length(); j++) {
-                string b = string() + s[j] + s[j+1];
-                if ((a == "AB" && b == "BA") || (a == "BA" && b == "AB")) {
-                    cout << "YES" << endl;
-                    return 0;
-                }
-            }
-        }
+        string z = string() + s[i] + s[i+1];
+        if (z == "AB")
+            a.push_back(i);
+        else if (z == "BA") 
+            b.push_back(i);
     }
-    cout << "NO" << endl;
+
+    if (a.empty() || b.empty()) {
+        cout << "NO" << endl;
+    }
+    else if (*max_element(a.begin(), a.end()) - *min_element(b.begin(), b.end()) > 1) {
+        cout << "YES" << endl;
+    }
+    else if (*max_element(b.begin(), b.end()) - *min_element(a.begin(), a.end()) > 1) {
+        cout << "YES" << endl;
+    }
+    else {
+        cout << "NO" << endl;
+    }
+
     return 0;
 }
